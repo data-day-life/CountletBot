e2e96363dcc467a5bff5fc2b12ac38fd9a4e2630
Paul Jamison <pjamison7@gatech.edu>
2023-12-06 13:54:31 -0800

    bot gets count history

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,23 +1,28 @@
 import os
 import random
 import discord
+import helpers
 
 token = os.getenv("CLIENT_TOKEN")
-# my_guild = os.getenv("GUILD_ID")
+count_guild_id = os.getenv("GUILD_ID")
+count_chan_id = os.getenv('COUNT_CHAN_ID')
 # my_guild = 780312283233779713
 guild_name = 'Kaiyacord'
-count_chan = os.getenv('COUNT_CHAN_ID')
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == guild_name:
-            print(
-                f"{client.user} is connected to the following guild:\n"
-                f"{guild.name}(id: {guild.id})"
-            )
+    # Now that the bot is connected, do something
+    count_guild = await client.fetch_guild(count_guild_id)
+    count_channel = await count_guild.fetch_channel(count_chan_id)
+
+    starting_count_msg_id = 1053012296370229248
+    search_after = count_channel.get_partial_message(starting_count_msg_id).created_at
+    print('Fetching count history from count channel')
+    messages = await helpers.get_count_history(count_channel, search_after)
+
+    pass
 
 client.run(token)
--- src/bot/helpers.py
+++ src/bot/helpers.py
@@ -1,11 +1,29 @@
 # Gets all message history for a given channel
-async def get_msg_history(channel):
+
+starting_count_msg_id = 1050903420439101471
+
+async def get_count_history(channel, search_after):
     all_messages = []
-    async for msg in channel.history(limit=100,
+    start_count = None
+    # Fetch all messages, starting from most recent
+    async for msg in channel.history(limit=None,
                                      before=None,
-                                     after=None,
+                                     after=search_after,
                                      around=None,
-                                     oldest_first=None):
+                                     oldest_first=True):
+        print(msg.content)
         all_messages.append(msg)
+        # Check message content to see if it contains the number `1` (and only the number `1`)
+        # if get_latest_start_count(msg):
+        #     start_count = msg
+        #     print(f'\n***Start Count: {start_count.content}\n')
+        #     break
     return all_messages
+
+
+def get_latest_start_count(message):
+    if message.content.startswith('1 '):
+        return message
+    return None
+
 
