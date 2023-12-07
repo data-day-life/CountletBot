ff423507084379a8d0754165ec2d949424be1710
Paul Jamison <pjamison7@gatech.edu>
2023-12-07 14:05:29 -0800

    tiny refactor

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,6 +1,6 @@
+import src.config as cfg
 import discord
 import helpers
-import src.config as config
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -9,14 +9,14 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     # Now that the bot is connected, do something
-    count_guild = helpers.get_count_guild(client, config.GUILD_ID)
-    count_channel = helpers.get_count_channel(count_guild, config.COUNT_CHAN_ID)
+    count_guild = helpers.get_count_guild(client, cfg.GUILD_ID)
+    count_channel = helpers.get_count_channel(count_guild, cfg.COUNT_CHAN_ID)
 
     print('Fetching count history from count channel...')
-    search_after = helpers.get_msg_to_search_after(count_channel, config.SEARCH_AFTER_MSG_ID)
+    search_after = helpers.get_msg_to_search_after(count_channel, cfg.SEARCH_AFTER_MSG_ID)
     messages = await helpers.get_count_history(count_channel, search_after)
 
     pass
 
 
-client.run(token)
+client.run(cfg.CLIENT_TOKEN)
