f5b2734ca1cfbf3bec33ce1f5d3fa0918b4754de
Paul Jamison <pjamison7@gatech.edu>
2023-12-08 12:07:41 -0800

    still shaping request results

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,9 +1,35 @@
 import src.config as cfg
 import discord
 import helpers
+import asyncio
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+
+
+async def cold_boot(client, **kwargs):
+    """ Connects to a guild and counting channel, collects the message history and saves it.
+
+    :param client: a discord.Client object
+    :return: the collected messages from a discord server's counting channel.
+    """
+    count_guild = await helpers.get_count_guild(client, cfg.GUILD_ID, **kwargs)
+    count_channel = await helpers.get_count_channel(count_guild, cfg.COUNT_CHAN_ID, **kwargs)
+    search_after = helpers.get_msg_datetime(count_channel, cfg.SEARCH_AFTER_MSG_ID)
+
+    messages = await helpers.get_count_history(count_channel, search_after, **kwargs)
+    parsed_msgs = helpers.parse_channel_messages(messages, **kwargs)
+
+    result = {'count_guild': count_guild,
+              'count_channel': count_channel,
+              'search_after': search_after,
+              'messages': messages,
+              'parsed_msgs': parsed_msgs,
+              }
+    # Save the messages to a file.
+    helpers.write_pickle_results(parsed_msgs['messages'], 'cold_boot_count_history.pk', **kwargs)
+    # helpers.save_channel_msgs_to_json(parsed_msgs['messages'], filename='cold_boot_count_history.json', **kwargs)
+    return result
 
 
 @client.event
