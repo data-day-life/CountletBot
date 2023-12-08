85467613eeba5c22a365dfc84aef8a15fc37fd94
Paul Jamison <pjamison7@gatech.edu>
2023-12-08 12:06:47 -0800

    expanded helpers

--- src/bot/helpers.py
+++ src/bot/helpers.py
@@ -1,6 +1,9 @@
 # Gets all message history for a given channel
 import json
 import pickle
+import time
+from datetime import timedelta
+from typing import List, Any
 
 
 async def get_count_guild(client, count_guild_id, **kwargs):
@@ -34,19 +37,49 @@ def get_msg_datetime(count_channel, msg_id):
     return msg_dt
 
 
+def parse_channel_message(msg, **kwargs):
+    message = {'msg_id': int(msg.id),
+               'auth_id': int(msg.author.id),
+               'content': str(msg.content),
+               'created_at': msg.created_at,
+               'edited_at': msg.edited_at if msg.edited_at else None,
+               # 'created_at': msg.created_at.timestamp(),
+               # 'edited_at': msg.edited_at.timestamp() if msg.edited_at else None,
+               'reactions': str(msg.reactions),
+               }
+    return message
+
+
 def parse_channel_messages(channel_msgs, **kwargs):
     """ Given a collection of channel messages, parse them into an appropriate format for db storage.
 
     :param channel_msgs: one or more channel messages from a channel to be formatted
     :return: a formatted collection of parsed channel messages.
     """
-
+    # <Message id=1053023864046751774 channel=<TextChannel id=1038808890466390066 name='the-quest-to-10k' position=2 nsfw=False news=False category_id=780312283746009128> type=<MessageType.default: 0> author=<User id=82982763317166080 name='ahi' global_name='Ahi' bot=False> flags=<MessageFlags value=0>>
     verbose = kwargs.get('verbose')
 
+    all_messages = []
+    server_channels: list[Any] = []
+    for msg in channel_msgs:
+        server_channel = {'guild_name': str(msg.guild.name),
+                          'guild_id': int(msg.guild.id),
+                          'count_chan_name': str(msg.channel.name),
+                          'count_chan_id': int(msg.channel.id),
+                          'last_msg_id': int(msg.channel.last_message_id),
+                          'updated_at': ''
+                          }
+        server_channels.extend([server_channel] if server_channel not in server_channels else [])
+        message = parse_channel_message(msg, **kwargs)
 
-def save_channel_msgs_to_json(channel_msgs, filename='count_history.json', **kwargs):
+        all_messages.append(message)
+    return {'messages': all_messages, 'servers': server_channels}
+
+
+def save_channel_msgs_to_json(channel_msgs, filename='count_history.json', folder='data/', **kwargs):
     """ Saves a collection of channel messages to a local json file.
 
+    :param folder:
     :param channel_msgs: a collection of channel messages from the message history
     :param filename: the name of the filename where channel messages should be saved
     :param kwargs: verbose
@@ -54,16 +87,17 @@ def save_channel_msgs_to_json(channel_msgs, filename='count_history.json', **kwa
     """
     verbose = kwargs.get('verbose')
     if verbose:
-        print(f'Now saving {len(channel_msgs)} channel messages as:  `{filename}`')
-    with open(filename, 'w', encoding='utf-8') as fh:
+        print(f'Now saving {len(channel_msgs)} channel messages as:  `data/{filename}`')
+    with open(folder + filename, 'w', encoding='utf-8') as fh:
         json.dump(channel_msgs, fh, ensure_ascii=False, indent=4)
     if verbose:
-        print(f'  Success!  Saved {len(channel_msgs)} messages to:  `{filename}`')
+        print(f'  Success!  Saved {len(channel_msgs)} messages to:  `data/{filename}`')
 
 
-def write_pickle_results(some_results, filename, **kwargs):
+def write_pickle_results(some_results, filename, folder='data/', **kwargs):
     """ Saves a collection of channel messages to a local json file.
 
+    :param folder:
     :param some_results: results after using a client to fetch some messages
     :param filename: the name of the pickle file to save as, including extension
     :param kwargs: verbose
@@ -71,27 +105,28 @@ def write_pickle_results(some_results, filename, **kwargs):
     """
     verbose = kwargs.get('verbose')
     if verbose:
-        print(f'Saving results to: `{filename}`')
-    with open(filename, 'wb') as fh:
+        print(f'Saving results to: `data/{filename}`')
+    with open(folder + filename, 'wb') as fh:
         pickle.dump(some_results, fh, protocol=pickle.HIGHEST_PROTOCOL)
         if verbose:
-            print(f'  Success!  Saved results to: `{filename}`')
+            print(f'  Success!  Saved results to: `data/{filename}`')
 
 
-def load_pickle_results(filename, **kwargs):
+def load_pickle_results(filename, folder='data/', **kwargs):
     """ Loads a pickled object from a local file..
 
+    :param folder:
     :param filename: the name of the pickle file to load from, including extension
     :param kwargs: verbose
     :return: result
     """
     verbose = kwargs.get('verbose')
     if verbose:
-        print(f'Loading results from: `{filename}`')
-    with open(filename, 'rb') as fh:
+        print(f'Loading results from: `data/{filename}`')
+    with open(folder + filename, 'rb') as fh:
         result = pickle.load(fh)
         if verbose:
-            print(f'  Success!  Loaded results from: `{filename}`')
+            print(f'  Success!  Loaded results from: `data/{filename}`')
     return result
 
 
@@ -100,8 +135,9 @@ async def get_count_history(count_channel, search_after, **kwargs):
     verbose = kwargs.get('verbose')
     if verbose:
         print('Fetching count history from count channel...')
+        start = time.time()
     # Fetch all messages
-    async for msg in count_channel.history(limit=None,
+    async for msg in count_channel.history(limit=250,
                                            before=None,
                                            after=search_after,
                                            around=None,
@@ -109,6 +145,7 @@ async def get_count_history(count_channel, search_after, **kwargs):
         all_messages.append(msg)
     if verbose:
         print(f'  Success!  Fetched {len(all_messages)} messages from counting channel.')
+        print(f'    took {str(timedelta(seconds=(time.time() - start)))} sec.')
     return all_messages
 
 
