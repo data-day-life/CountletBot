85090d2acd734fc5967a9a902dfa580783decb6e
Paul Jamison <pjamison7@gatech.edu>
2024-04-30 14:06:19 -0700

    bot: just do cold_boot

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -9,13 +9,13 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     # Now that the bot is connected, do something
-    count_guild = await helpers.get_count_guild(client, cfg.GUILD_ID)
-    count_channel = await helpers.get_count_channel(count_guild, cfg.COUNT_CHAN_ID)
-
-    print('Fetching count history from count channel...')
-    search_after = helpers.get_msg_datetime(count_channel, cfg.SEARCH_AFTER_MSG_ID)
-    messages = await helpers.get_count_history(count_channel, search_after)
-
+    # count_guild = await helpers.get_count_guild(client, cfg.GUILD_ID)
+    # count_channel = await helpers.get_count_channel(count_guild, cfg.COUNT_CHAN_ID)
+    #
+    # print('Fetching count history from count channel...')
+    # search_after = helpers.get_msg_datetime(count_channel, cfg.SEARCH_AFTER_MSG_ID)
+    # messages = await helpers.get_count_history(count_channel, search_after)
+    await helpers.cold_boot(client)
     pass
 
 
